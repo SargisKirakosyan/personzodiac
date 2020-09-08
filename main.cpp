0a49ab16393042f14269a83467c2329ddbc3fe2a
SargisKirakosyan <sargiskirakosyan.wrk@gmail.com>
2020-09-08 18:57:02 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,13 +4,13 @@ int main() {
   int personQuantity;
   std::cin >> personQuantity;
   personYear::Person** person = new personYear::Person*[personQuantity];
-  for (int i=0; i<personQuantity; ++i) {
+  for (int i = 0; i < personQuantity; ++i) {
     std::string fn, ln;
     double by, bm, bd;
     std::cin >> fn >> ln >> by >> bm >> bd;
     person[i] = new personYear::Person(fn, ln, by, bm, bd);
   }
-  for(int j=0; j<personQuantity; j++) {
+  for(int j = 0; j < personQuantity; j++) {
     std::cout << person[j]->getYearAnimal() << ", " << person[j]->getZodiac() << std::endl;
   }
 }
