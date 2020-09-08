0427fefd9a821dd1d2535b52a11924ea1d49bb3a
SargisKirakosyan <sargiskirakosyan.wrk@gmail.com>
2020-09-08 16:10:43 +0400

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -1,164 +1,125 @@
-#include <iostream>
-#include <cmath>
+#include "person.h"
 
-namespace personYear {
-  class Person {
-    private:
-      std::string firstName, lastName;
-      double birthYear;
-      int birthMonth, birthDay;
-    public:
-      Person(std::string fn, std::string ln, int by, int bm, int bd) : firstName(fn), lastName(ln), birthYear(by), birthMonth(bm), birthDay(bd)  {}
-      ~Person(){};
-      std::string checkYearAnimal() {
-        std::string yearAnimal;
-        double whole, decimal;
-        decimal = std::modf(birthYear/12, &whole);
-        int year = decimal*12;
-        switch(year) {
-          case 0:
-            yearAnimal="Կապիկ";
-            break;
-          case 1:
-            yearAnimal="Աքլոր";
-            break;
-          case 2:
-            yearAnimal="Շուն";
-            break;
-          case 3:
-            yearAnimal="Խոզ";
-            break;    
-          case 4:
-            yearAnimal="Առնետ";
-            break;
-          case 5:
-            yearAnimal="Ցուլ";
-            break;    
-          case 6:
-            yearAnimal="Վագր";
-            break;    
-          case 7:
-            yearAnimal="Ճագար";
-            break;    
-          case 8:
-            yearAnimal="Վիշապ";
-            break;    
-          case 9:
-            yearAnimal="Օձ";
-            break;    
-          case 10:
-            yearAnimal="Ձի";
-            break;
-          case 11:
-            yearAnimal="Այծ";
-            break;
-        }
-        return yearAnimal;
-      }
-      std::string checkZodiac() {
-        std::string animalName;
-        switch(birthMonth) {
-          case 1:
-            if(birthDay>=20) {
-            animalName="Ջրհոս";
-            } else {
-              animalName="Այծեղջյուր";
-            }
-            break;
-          case 2:
-            if(birthDay<19) {
-              animalName="Ջրհոս";
-            } else {
-              animalName="Ձկներ";
-            }
-            break;
-          case 3:
-            if(birthDay<=20) {
-              animalName="Ձկներ";
-            } else {
-              animalName="Խոյ";
-            }
-            break;
-          case 4:
-            if(birthDay<=20) {
-              animalName="Խոյ";
-            } else {
-              animalName="Ցուլ";
-            }
-            break;
-          case 5:
-            if(birthDay>=20) {
-              animalName="Ցուլ";
-            } else {
-              animalName="Երկվորյակներ";
-            }
-            break;
-          case 6:
-            if(birthDay<=21) {
-              animalName="Երկվորյակներ";
-            } else {
-              animalName="Խեցգետին";
-            }
-            break;
-          case 7:
-            if(birthDay<=22) {
-              animalName="Խեցգետին";
-            } else {
-              animalName="Առյուծ";
-            }
-            break;
-          case 8:
-            if(birthDay<=23) {
-              animalName="Առյուծ";
-            } else {
-              animalName="Կույս";
-            }
-            break;
-          case 9:
-            if(birthDay<=23) {
-              animalName="Կույս";
-            } else {
-              animalName="Կշեռք";
-            }
-            break;
-          case 10:
-            if(birthDay<=23) {
-              animalName="Կշեռք";
-            } else {
-              animalName="Կարիճ";
-            }
-            break;
-          case 11:
-            if(birthDay<=22) {
-              animalName="Կարիճ";
-            } else {
-              animalName="Աղեղնավոր";
-            }
-            break;
-          case 12:
-            if(birthDay<=21) {
-              animalName="Աղեղնավոր";
-            } else {
-              animalName="Այծեղջյուր";
-            }
-            break;
-        }
-        return animalName;
-      }
-  };
+personYear::Person::Person(std::string fn, std::string ln, int by, int bm, int bd) : firstName(fn), lastName(ln), birthYear(by), birthMonth(bm), birthDay(bd)  {}
+
+std::string personYear::Person::getYearAnimal() {
+  double whole, decimal;
+  decimal = std::modf(birthYear/12, &whole);
+  int year = decimal*12;
+  switch(year) {
+    case 0:
+      return "Կապիկ";
+      break;
+    case 1:
+      return "Աքլոր";
+      break;
+    case 2:
+      return "Շուն";
+      break;
+    case 3:
+      return "Խոզ";
+      break;    
+    case 4:
+      return "Առնետ";
+      break;
+    case 5:
+      return "Ցուլ";
+      break;    
+    case 6:
+      return "Վագր";
+      break;    
+    case 7:
+      return "Ճագար";
+      break;    
+    case 8:
+      return "Վիշապ";
+      break;    
+    case 9:
+      return "Օձ";
+      break;    
+    case 10:
+      return "Ձի";
+      break;
+    case 11:
+      return "Այծ";
+      break;
+  }
+  return 0;
 }
-
-int main() {
-  int personQuantity;
-  std::cin >> personQuantity;
-  personYear::Person** person = new personYear::Person*[personQuantity];
-  for (int i=0; i<personQuantity; ++i) {
-    std::string fn, ln;
-    double by, bm, bd;
-    std::cin >> fn >> ln >> by >> bm >> bd;
-    person[i] = new personYear::Person(fn, ln, by, bm, bd);
+std::string personYear::Person::getZodiac() {
+  switch(birthMonth) {
+    case 1:
+      if(birthDay>=20) {
+        return "Ջրհոս";
+      }
+      return "Այծեղջյուր";
+      break;
+    case 2:
+      if(birthDay<19) {
+        return "Ջրհոս";
+      }
+      return "Ձկներ";
+      break;
+    case 3:
+      if(birthDay<=20) {
+        return "Ձկներ";
+      }
+      return "Խոյ";
+      break;
+    case 4:
+      if(birthDay<=20) {
+        return "Խոյ";
+      }
+      return "Ցուլ";
+      break;
+    case 5:
+      if(birthDay>=20) {
+        return "Ցուլ";
+      }
+      return "Երկվորյակներ";
+      break;
+    case 6:
+      if(birthDay<=21) {
+        return "Երկվորյակներ";
+      }
+      return "Խեցգետին";
+      break;
+    case 7:
+      if(birthDay<=22) {
+        return "Խեցգետին";
+      }
+      return "Առյուծ";
+      break;
+    case 8:
+      if(birthDay<=23) {
+        return "Առյուծ";
+      }
+      return "Կույս";
+      break;
+    case 9:
+      if(birthDay<=23) {
+        return "Կույս";
+      }
+      return "Կշեռք";
+      break;
+    case 10:
+      if(birthDay<=23) {
+        return "Կշեռք";
+      }
+      return "Կարիճ";
+      break;
+    case 11:
+      if(birthDay<=22) {
+        return "Կարիճ";
+      }
+      return "Աղեղնավոր";
+      break;
+    case 12:
+      if(birthDay<=21) {
+        return "Աղեղնավոր";
+      }
+      return "Այծեղջյուր";
+      break;
   }
-  for(int j=0; j<personQuantity; j++) {
-    std::cout << person[j]->checkYearAnimal() << " " << person[j]->checkZodiac() << std::endl;
-  }
+  return 0;
 }
